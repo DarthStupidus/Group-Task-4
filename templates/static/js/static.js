1b6eb77cd02d46b51b085725af5b5f4f8e704643
jamieah80 <jamiea.hanson80@gmail.com>
2024-07-21 14:08:00 +0100

    Updated Front end files, further testing still required

--- templates/static/js/static.js
+++
@@ -1,9 +0,0 @@
-let slider = document.getElementById("arange");
-let value = document.querySelector('.value');
-
-value.innerHTML = slider.value;
-
-slider.addEventListener('input', function() {
-    value.textContent = this.value;
-});
-
